5310b9d4789786a5fcfc2bf1597c01834e466b46
Jesse James <jjames@fearless.tech>
2022-04-21 20:03:33 -0400

    first commit

---
+++ packages/ds-macpro-gov/README.md
@@ -0,0 +1,31 @@
+# MACPRO Design System
+
+[![npm](https://img.shields.io/npm/v/@cmsgov/ds-medicare-gov.svg?label=@cmsgov%2Fds-medicare-gov)](https://www.npmjs.com/package/@cmsgov/ds-medicare-gov)
+
+[>> **View the full documentation site here** <<](https://cmsgov.github.io/design-system/medicare/)
+
+The _MACPRO Design System_ contains shared design and front-end development resources for Medicare.gov applications, and is built on top of the [CMS Design System](https://design.cms.gov/) (CMSDS). As a _child design system_, it inherits base styles, components, and guidance from the CMS Design System, while also adding its own features and customizations.
+
+## Usage
+
+`npm install testestest`
+
+For full documentation on installation and usage in your Medicare.gov product, please refer to [our documentation site](https://cmsgov.github.io/design-system/medicare/startup/installation/).
+
+## Contributing
+
+This is a design system established for MACPRO developers and designers to consume. It contains very similar styling to core CMSDS with added components such as multi-select dropdown.
+
+## Development
+
+See the [root CMSDS README](../../README.md).
+
+## Design assets
+
+One bigger difference to MACPRO's Design System is the usage of Figma as a source of design truth. [You can find that here](https://test)
+
+---
+
+### Additional links
+
+- For more information on the original Design System, check out [its GitHub page](https://github.com/cmsgov/design-system).
---
+++ packages/ds-macpro-gov/src/components/MultiSelectDropDown.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { Button } from '@cmsgov/design-system';
+import Select, { components } from 'react-select';
+
+const customComponents = {
+  MenuList: (props) => (
+    <div role="list">
+      <components.MenuList {...props} />
+    </div>
+  ),
+  Option: (props) => (
+    <div aria-selected={false} role="option">
+      <components.Option {...props} />
+    </div>
+  ),
+};
+
+const customStyles = {
+  multiValue: (provided) => ({
+    ...provided,
+    backgroundColor: '#0071BC',
+    color: '#fff',
+    fontWeight: 'bold',
+  }),
+  multiValueLabel: (provided) => ({
+    ...provided,
+    color: '#fff',
+  }),
+};
+
+export const MultiSelectDropDown = ({
+  ariaLabel,
+  cancelFn,
+  errorMessage,
+  loading = false,
+  onlyOne = false,
+  options,
+  placeholder,
+  required = false,
+  submitFn,
+  title,
+}) => {
+  const [value, setValue] = useState([]);
+
+  const invalid = (required && value.length === 0) || (onlyOne && value.length > 1);
+
+  return (
+    <div className="multi-select-dropdown-container">
+      <label className="ds-text-heading--xl">{title}</label>
+      <Select
+        aria-label={ariaLabel}
+        components={customComponents}
+        isDisabled={loading}
+        isMulti={!onlyOne}
+        onChange={setValue}
+        options={options}
+        placeholder={placeholder}
+        styles={customStyles}
+      />
+      <div className="multi-select-bottom-section">
+        {errorMessage && invalid && (
+          <div className="multi-select-error-message">{errorMessage}</div>
+        )}
+        <div className="multi-select-button-container">
+          <Button
+            disabled={loading || invalid}
+            onClick={() => submitFn(value)}
+            type="button"
+            variation="primary"
+          >
+            {loading && (
+              <>
+                <span
+                  className="ds-c-spinner ds-c-spinner-small ds-c-spinner--inverse"
+                  aria-valuetext="Loading"
+                  role="progressbar"
+                />
+                &nbsp;
+              </>
+            )}
+            Submit
+          </Button>
+          {cancelFn && (
+            <Button onClick={cancelFn} type="button" variation="transparent">
+              Cancel
+            </Button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
